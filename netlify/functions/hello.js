17947b4c6f23c9826064d4436bbee350170ff0a5
Omer Grinboim <omer364@gmail.com>
2025-02-23 19:50:58 +0200

    test18

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,23 +1,8 @@
-// hello there!
-// 
-// I'm a serverless function that you can deploy as part of your site.
-// I'll get deployed to AWS Lambda, but you don't need to know that. 
-// You can develop and deploy serverless functions right here as part
-// of your site. Netlify Functions will handle the rest for you.
+await import('hud-sdk/register')
 
-(async () => {
-    await import('hud-sdk/register'); // Dynamically import ESM module
-  })();
-  
-  
-const flowA = require('./flowA.cjs');
-// import { flowA } from './flowA.cjs';
-
-export const handler = async event => {
-    const subject = event.queryStringParameters.name || 'World'
-    const res = await flowA.startFlowA(5);
-    return {
-        statusCode: 200,
-        body: `Hello ${subject}! result: ${res}, ${process.env.AWS_LAMBDA_FUNCTION_MEMORY_SIZE}, ${process.env.AWS_LAMBDA_RUNTIME_API}`,
-    }
+export async function handler(event) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello, world!" }),
+  };
 }
