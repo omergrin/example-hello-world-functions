fe6b8f3464e5e8fe7e21db374762e90966ff7de9
Omer Grinboim <omer364@gmail.com>
2025-02-23 19:20:18 +0200

    test3

--- netlify/functions/hello.mjs
+++ netlify/functions/hello.mjs
@@ -3,4 +3,4 @@ await import('hud-sdk/register');
 import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
 
-require('./hello/hello.js')
+require('./hello/hell.js')
