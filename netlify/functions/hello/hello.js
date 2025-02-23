4c7dd1c56accbd8df08585b11c2e5dd55ebc3904
Omer Grinboim <omer364@gmail.com>
2025-02-23 19:12:25 +0200

    test

---
+++ netlify/functions/hello/hello.js
@@ -0,0 +1,20 @@
+// hello there!
+// 
+// I'm a serverless function that you can deploy as part of your site.
+// I'll get deployed to AWS Lambda, but you don't need to know that. 
+// You can develop and deploy serverless functions right here as part
+// of your site. Netlify Functions will handle the rest for you.
+
+
+  
+const flowA = require('./flowA.js');
+
+
+exports.handler = async event => {
+    const subject = event.queryStringParameters.name || 'World'
+    const res = await flowA.startFlowA(5);
+    return {
+        statusCode: 200,
+        body: `Hello ${subject}! result: ${res}, ${process.env.AWS_LAMBDA_FUNCTION_MEMORY_SIZE}, ${process.env.AWS_LAMBDA_RUNTIME_API}`,
+    }
+}
