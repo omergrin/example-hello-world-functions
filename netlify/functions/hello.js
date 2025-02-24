899b370511af6784e90dc773ed224523d8942134
Omer Grinboim <omer364@gmail.com>
2025-02-24 20:05:52 +0200

    a

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -4,10 +4,10 @@ const flowA = require('./hello/flowA.js');
 async function handler(event) {
 
     const subject = event.queryStringParameters.name || 'World'
-    const res = await flowA.startFlowA(5);
+    const res = await flowA.startFlowA(3);
     return {
         statusCode: 200,
-        body: `Hello $x{subject}! result: ${res}, ${process.env.AWS_LAMBDA_FUNCTION_MEMORY_SIZE}, ${process.env.AWS_LAMBDA_RUNTIME_API}`,
+        body: `Hello ${subject}! result: ${res}, ${process.env.AWS_LAMBDA_FUNCTION_MEMORY_SIZE}, ${process.env.AWS_LAMBDA_RUNTIME_API}`,
     }
 }
 
