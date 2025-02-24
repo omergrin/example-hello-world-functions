a61cf8e127a185c61655b37f0c389134f1349437
Omer Grinboim <omer364@gmail.com>
2025-02-24 20:02:01 +0200

    a

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,5 +1,3 @@
-// require('hud-sdk')
-// import { flowA } from './hello/flowA.js';
 
 const flowA = require('./hello/flowA.js');
 
@@ -14,21 +12,3 @@ async function handler(event) {
 }
 
 module.exports = { handler }; 
-
-
-// (async () => {
-//     await import('hud-sdk/register'); // Dynamically import ESM module
-//   })();
-  
-  
-// const flowA = require('./flowA.cjs');
-// // import { flowA } from './flowA.cjs';
-
-// export const handler = async event => {
-//     const subject = event.queryStringParameters.name || 'World'
-//     const res = await flowA.startFlowA(5);
-//     return {
-//         statusCode: 200,
-//         body: `Hello ${subject}! result: ${res}, ${process.env.AWS_LAMBDA_FUNCTION_MEMORY_SIZE}, ${process.env.AWS_LAMBDA_RUNTIME_API}`,
-//     }
-// }
